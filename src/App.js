e24205d096db8d129562217b0d9581386aa3dfef
Aourfi Ossama <aourfidev@gmail.com>
2024-12-27 16:17:49 +0100

    PlansComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programms from './components/Programms/Programms';
 import Reasons from './components/Reasons/Reasons';
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <div className="App">
       <Hero />
       <Programms />
-      <Reasons/>
+      <Reasons />
+      <Plans/>
     </div>
   );
 }
